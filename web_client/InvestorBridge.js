3eb6712283bc46b30d79ae6dbcdf81f6f3e7e147
chengaobo <chengaobo@rijin.com>
2016-05-16 16:50:34 +0800

    Update InvestirBridge.js

--- web_client/InvestorBridge.js
+++ web_client/InvestorBridge.js
@@ -1,12 +1,22 @@
+/*
+ * InvestorBridge for react-native based investor_react_ios
+ * author:oboochin
+ * email:oboochin@gmail.com 
+**/
+
 'use strict';
 
 (function( window ){
+
+	if( window.RJMessage ) return;
+
 	var RJBridge = {};
 	var tagMessage = "RJMessage::";
 	var methods = {
 		navigate:'navigate::',
 		popUp:'popUp::',
-		toast:'toast::'
+		toast:'toast::',
+		share:'share::',
 	}
 
 	function changeHash( method ,content ){
@@ -25,6 +35,23 @@
 		return tagMessage + methods[ method ] + paramString;
 	}
 
+	/*
+	* @func popUp
+	* @param content object 
+	* 	Main content showen in the popUp mathod
+	* @param configs
+	*	popUp window configs.
+	* @example 
+	* 	RJBridge.popUp('Hello Nancy',{
+	*		title:'Greeting',
+	*		buttons:[
+	*			['cancel',function(){ console.log('cancel') }],
+	*			['Hello',function(){ console.log('Hello') }]
+	*	 	]
+	*	})
+	**/
+
+
 	RJBridge.popUp = function( content, configs ){
 		if( !content ) return;
 
@@ -35,7 +62,19 @@
 		changeHash( 'popUp', JSON.stringify(configParams) );
 	}
 
-	RJBridge.navigate = function( targetRoute, params ){
+	/*
+	* @func push
+	* @param targetRoute string 
+	* 	route want to push.Been listed at investor_react_ios/app/components/RouteMap.js.
+	* @param params
+	*	params transer to next route.
+	* @example 
+	* 	RJBridge.push('login/login',{
+	*		user_id:12,
+	*	})
+	**/
+
+	RJBridge.push = function( targetRoute, params ){
 		var route = {};
 		route.targetRoute = targetRoute;
 
@@ -49,8 +88,31 @@
 		changeHash( 'navigate', { route:route } );
 	}
 
+
+	/*
+	* @func toast
+	* @param message string 
+	* 	toast message.
+	* @example 
+	* 	RJBridge.share( 'error here.' )
+	**/
+
 	RJBridge.toast = function( message ){
 		changeHash( 'toast', { message:message } );
+	}
+
+
+	/*
+	* @func share
+	* @param shareUrl string 
+	* 		url want to share.
+	* @example 
+	* 	RJBridge.share( window.location.href )
+	**/
+
+	RJBridge.share = function( shareUrl ){
+
+		changeHash( 'share', { shareUrl:shareUrl } );
 	}
 
 	window.RJMessage = RJBridge;
